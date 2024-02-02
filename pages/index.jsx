6ee29d638ba27c4f0864f3a656d51405e824a20f
Denis Petrushkov <dpetrushkov@markupus.tech>
2024-02-02 13:39:31 +0200

    added GameTitle to HomePage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,13 @@
-import Header from "../components/header";
+import { GameTitle } from "../components/game";
+import { Header } from "../components/header";
 
 export default function HomePage() {
   return (
     <div className="bg-slate-50 min-h-screen">
       <Header />
+      <main className="pt-6 mx-auto max-w-[616px]">
+        <GameTitle />
+      </main>
     </div>
   );
 }
