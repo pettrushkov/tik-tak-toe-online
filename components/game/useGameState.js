e2e10c8948f41bc76738fbcb7fc10e4aa5de8ae4
Denis Petrushkov <dpetrushkov@markupus.tech>
2024-02-08 12:49:13 +0200

    added computeWinner logic to useGameState

--- components/game/useGameState.js
+++ components/game/useGameState.js
@@ -8,11 +8,42 @@ function getNextMove(currentMove, playersCount) {
   return sliceMoveOrder[nextMoveIndex] ?? sliceMoveOrder[0];
 }
 
+function computeWinner(cells, sequenceSize = 5, fieldSize = 19) {
+  const gap = Math.floor(sequenceSize / 2);
+
+  function getSequenceIndexes(i) {
+    const res = [
+      [], // get indexes by -
+      [], // get indexes by |
+      [], // get indexes by /
+      [], // get indexes by \
+    ];
+
+    for (let j = 0; j < sequenceSize; j++) {
+      res[0].push(j - gap + i);
+      res[1].push(fieldSize * (j - gap) + i);
+      res[2].push(-fieldSize * (j - gap) + (j - gap) + i);
+      res[3].push(fieldSize * (j - gap) + (j - gap) + i);
+    }
+
+    return res;
+  }
+
+  for (let i = 0; i < cells.length; i++) {
+    if (cells[i]) {
+      console.log(i);
+      console.log(getSequenceIndexes(i));
+    }
+  }
+}
+
 export function useGameState(playersCount) {
   const [{ cells, currentMove }, setGameState] = useState(() => ({
     cells: new Array(19 * 19).fill(null),
     currentMove: GAME_SYMBOLS.CROSS,
   }));
+
+  computeWinner(cells);
 
   const nextMove = getNextMove(currentMove, playersCount);
 
